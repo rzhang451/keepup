67d5dd092e61e75e25edd66b1ed9fe508e123386
Xinqing <75204105+Ewiggestrige@users.noreply.github.com>
2021-03-15 19:58:52 +0100

    Update courseRouter.js

--- Back-end/routers/courseRouter.js
+++ Back-end/routers/courseRouter.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
-const recommandation = require('../controller/recommandationCtrl.js');
+const course = require('../controller/courseCtrl.js');
 
-router.get('/course',recommandation.courseRecommandation);
-router.get('/homepage/search-with-parameters',recommandation.searchWithParameters);
+router.get('/',course.allCourse); //to show all the courses
 
-router.get('/course/')
+router.get('/my-recommandation',course.myRecommandation);//to search my course with parameters
+router.get('/my-recommandation/course-recommand',course.courseRecommand);
+
+router.get('/video/:vnumber',course.video);//to show video
+router.get('/video/:vnumber/fullscreen',course.fullscreen);
+router.get('/video/:vnumber/comment/:id',course.comment);
+
+router.get('/my-course',course.myCourse);//to show my agenda and all course I have done.
+router.get('/my-course/favorite',course.favorite);
+
 
 module.exports = router;
