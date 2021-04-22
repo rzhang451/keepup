788867abc36f446ec06c87c2b83c7e3288fed075
ysong02 <71662449+ysong02@users.noreply.github.com>
2021-04-22 19:36:27 +0200

    Update mongoose.js

--- Back-end/database/mongoose.js
+++ Back-end/database/mongoose.js
@@ -1,8 +1,9 @@
 //引入mongoose
 const mongoose = require("mongoose");
 //链接数据库
-const address = "mongodb+srv://nanachi:36070876@cluster0.nfeev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const address = "mongodb+srv://nanachi:36070876@cluster0.nfeev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; //刘
 //mongoose.connect(address,{useNewUrlParser:true,useUnifiedTopology:true},()=>console.log("success!"));
+//"mongodb+srv://LJT_S:001021@cluster0.2xb7a.mongodb.net/db00?retryWrites=true&w=majority" //宋
 mongoose.connect(address);
 var db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error:'));
