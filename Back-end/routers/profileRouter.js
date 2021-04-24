c173111e98beb027be5d3dffb8fb21c3748e8d7b
Xinqing <75204105+Ewiggestrige@users.noreply.github.com>
2021-04-24 20:49:11 +0200

    Update profileRouter.js
    add two functions: sign-up and sign-in

--- Back-end/routers/profileRouter.js
+++ Back-end/routers/profileRouter.js
@@ -1,7 +1,57 @@
+//引入express框架
 var express = require('express');
+//申明跳转路由
 var router = express.Router();
-const profile = require('../controller/profile.js');
+//声明引用数据库
+var Profile = require('../database/profileModel');
 
-router.get('/',profile.login);
+//sign-in 登陆
+router.get('/login/submit',function(req,res){
+  var person = new Profile({
+    username: req.query.usr,
+    password: req.query.pwd
+  });
+  Profile.find({username: person.username},(err,docs)=>{
+    if(err){
+      return console.error(err);
+    }
+    if(!docs.length){
+      res.send("Username incorrect!");
+    }else if(docs[0].password == person.password){
+      res.send("Login successful!");
+    }else{
+      res.send("Password incorrect!");
+    }
+  })
+});
+
+//sign-up 注册
+router.get('/login/sign-up/submit',function(req,res){
+  var person = new Profile({
+    username: req.query.usr,
+    password: req.query.pwd
+  });
+  Profile.find({username: person.username},(err,docs)=>{
+    if(!err){
+      if(!docs.length){
+        person.save(function(err,person){
+          if(err){
+            return console.error(err);
+          }else{
+            res.send("You have successfully signed up!");
+          }
+        });
+      }else{
+        res.send("Username already used!");
+      }
+    }else{
+      return console.error(err);
+    }
+  })
+});
+
+//levelTest
+
+
 
 module.exports = router;
