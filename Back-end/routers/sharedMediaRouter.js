7d9327cd2bc30692b901e009635f1384f0d69a09
ShawnYWY <ywy.shawn@gmail.com>
2021-04-24 22:43:03 +0200

    diary

--- Back-end/routers/sharedMediaRouter.js
+++ Back-end/routers/sharedMediaRouter.js
@@ -1,4 +1,18 @@
 var express = require('express');
-var app = express();
+var router = express.Router();
+var Profile = require('../database/profileModel');
+var SharedDiary = require('../database/diaryModel');
 
-module.exports = router;
+//example
+/*
+var Diary1 = new SharedDiary({
+    username：
+    context: ['今天天气真好'],
+    thumb: 0
+});
+Diary1.save(function(err,Diary1){
+    if(err) return console.error(err);
+    console.log("successfully added!");
+  });
+*/
+   
