d2182edad3222c55c0b2a23449384c373bd9ef68
liziyu0104 <67859412+liziyu0104@users.noreply.github.com>
2021-05-13 06:30:45 +0200

    Add files via upload

--- Back-end/database/myCourseModel.js
+++ Back-end/database/myCourseModel.js
@@ -5,7 +5,12 @@ var myCourseSchema = mongoose.Schema({
   usr: String,
   course: Number,
   favorite: Boolean,
-  schedul: String
+  schedul: [Number] //0-6代表周日到周六
 });
+
+//Methods
+
+
+
 //Model
 module.exports = mongoose.model('MyCourse',myCourseSchema);
