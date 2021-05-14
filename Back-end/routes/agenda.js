d2bb68eaa89e04c83b134001dd0c64375f5ce47b
Xinqing <75204105+Ewiggestrige@users.noreply.github.com>
2021-05-14 22:07:40 +0200

    Update agenda.js

--- Back-end/routes/agenda.js
+++ Back-end/routes/agenda.js
@@ -1,5 +1,3 @@
-const express = require('express');
-const router = express.Router();
 const Profile = require('../database/profileModel');
 const MyCourse = require('../database/myCourseModel');
 const Course = require('../database/courseModel');
