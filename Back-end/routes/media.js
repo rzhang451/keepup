08b4f47fff56a1698a88b515504e561d18ed3939
liziyu0104 <67859412+liziyu0104@users.noreply.github.com>
2021-05-15 01:27:53 +0200

    Add files via upload

---
+++ Back-end/routes/media.js
@@ -0,0 +1,47 @@
+const Profile = require('../model/profileModel');
+const Follow = require('../model/followModel');
+const Media = require('../model/mediaModel');
+
+exports.user = (req,res,next)=>{
+  Profile.findOne({id:req.params.id},(err,docs)=>{
+    if(err){
+      return res.json({
+        msg:'Failed to connect',
+        code: 'error'
+      });
+    }
+    if(!docs){
+      return res.json({
+        msg:'Username incorrect!',
+        code: 'error'
+      });
+    }
+    var profile = {
+      username: docs.username,
+      sex: docs.sex,
+      location: docs.location,
+      miniIntro: docs.miniIntro
+    };
+    var info = [];
+    Media.find({id:req.params.id},(err,actus)=>{
+      if(err){
+        return res.json({
+          msg:'Failed to connect',
+          code: 'error'
+        });
+      }
+      var i=0;
+      while(i<3&&i<actus.length){
+        info.append(actus[i]);
+        i++;
+      }
+    });
+    return res.json({
+      msg:'Profile is returned',
+      code:'success',
+      profile:profile,
+      info:info
+    });
+  });
+  next();
+}
