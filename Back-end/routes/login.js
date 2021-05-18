13811857631317051d62bd34b7f84a028682a292
Xinqing <75204105+Ewiggestrige@users.noreply.github.com>
2021-05-18 17:18:23 +0200

    Update login.js

--- Back-end/routes/login.js
+++ Back-end/routes/login.js
@@ -1,16 +1,16 @@
 const email = require('../funcs/nodemailer');
 const validator = require('validator');
-const uuid = require('node-uuid');
+const uuid = require('node-uuid');;
 
 var signUpCheck = {};
 var forgetPwdCheck = {};
-exports.sign-in = (req,res,next)=>{
+exports.sign_in = (req,res,next)=>{
   //假如已经登录
   if(req.session.usr){
     return res.json({
       msg:'Welcome',
-      code: 0,
-      info: docs[0].id
+      code: '200',
+      data:[{id:req.session.usr}]
     });
   }
   var email = req.body.email;
@@ -20,77 +20,80 @@ exports.sign-in = (req,res,next)=>{
     if(err){
       return res.json({
         msg:'Failed to connect',
-        code: 'error2'
+        code: '-1'
       });
     }
     if(!docs.length){
       //返回用户不存在
       return res.json({
         msg:'User doesn\'t existe!',
-        code: 'error1'
+        code: '201'
       });
     }else if(docs[0].password == person.password){
       //返回登录成功,返回用户id
       res.session.usr = docs[0].id;
       return res.json({
         msg:'Welcome',
-        code: 0,
-        info: docs[0].id
+        code: '200',
+        data: [{id:docs[0].id}]
       });
     }else{
       //返回密码错误
       return res.json({
         msg:'Password incorrect!',
-        code: 'error2',
+        code: '202',
       });
     } 
   })
   next();
 } 
 
-exports.sign-up_email = (req,res,next)=>{
+exports.sign_up_email = (req,res,next)=>{
   var mail = req.body.email;
   var code = parseInt((Math.random(0,1)*0.9+0.1)*10000);
   if(!validator.isEmail(email)){
       return res.json({
         msg:'This is not a email!',
-        code: 'error2'
-      });'
-  }
+        code: -1
+      });
+  }   
   Profile.find({email: mail},(err,docs)=>{
     if(!err){
-      if(!docs.length){
+      if(docs.length){
         return res.json({
           msg: 'Email already used!',
-          code: 'error1'
+          code: -1
         });
       }else{
         signUpCheck[mail]=code;
         email.sendMail(mail,'signup',code,(state)=>{
           if(state){
+            console.log('已发送');
             return res.json({
               msg:'Send Code Successful',
-              code:'success'
+              code:200
             });
           }else{
+            console.log('发送失败');
             return res.json({
               msg:'Send Code Failed',
-              code:'error2'
+              code:-1
             });
-          }
+          } 
         });
-      }
+      } 
     }else{
       return res.json({
         msg:console.error(err),
-        code:'error3'
+        code:-1
       });
-    }
+    } 
   })
   next();
-}
+} 
 
-exports.sign-up_code = (req,res,next)=>{
+
+exports.sign_up_code = (req,res,next)=>{
   var mail = req.body.email;
   var code = req.body.code;
   if(code == signUpCheck[mail]){
@@ -107,7 +110,7 @@ exports.sign-up_code = (req,res,next)=>{
   next();
 }
 
-exports.sign-up_pwd = (req,res,next)=>{
+exports.sign_up_pwd = (req,res,next)=>{
   if(req.body.pwd!=req.body.check){
     return res.json({
       msg:'passwords are not the same!',
@@ -118,17 +121,17 @@ exports.sign-up_pwd = (req,res,next)=>{
       return res.json({
         msg:'Password Invalide,the length should be between 6 and 12, please enter again',
         code: 'error2'
-      });'
+      });
   }
   var user = new Profile({
-    id = uuid.v1,
-    email = req.body.email,
-    password = req.body.pwd
+    id: uuid.v1,
+    email: req.body.email,
+    password: req.body.pwd
   });
   user.save(function(err,user){
     if(err){
       console.log(err);
-      return res.json{(
+      return res.json({
         msg:'Failed to Save User',
         code:'error3'
       )};
@@ -137,14 +140,14 @@ exports.sign-up_pwd = (req,res,next)=>{
       return res.json({
         msg:'Welcome',
         code:'success',
-        info:user.id
+        data:user.id
       });
     }
   });
   next();
 }
 
-exports.forget-pwd = (req,res,next)=>{
+exports.forget_pwd = (req,res,next)=>{
   var mail = req.body.email;
   var code = parseInt((Math.random(0,1)*0.9+0.1)*10000);
   if(!validator.isEmail(mail)){
@@ -186,7 +189,7 @@ exports.forget-pwd = (req,res,next)=>{
   next();
 }
 
-exports.forget-pwd_code = (req,res,next){
+exports.forget_pwd_code = (req,res,next){
   var mail = req.body.email;
   var code = req.body.code;
   if(!validator.matches(password,/(?!^\\d+$)(?!^[a-zA-Z]+$)(?!^[_#@]+$).{5,}/,'g') || !validator.isLength(password,6,12)){
@@ -209,7 +212,7 @@ exports.forget-pwd_code = (req,res,next){
   next();
 }
 
-exports.forget-pwd_pwd(req,res,next)=>{
+exports.forget_pwd_pwd=(req,res,next)=>{
   Profile.update({email:req.body.email},{$set:{password:req.body.pwd}},(err)=>{
     if(!err){
       Profile.findOne({email: mail},(err,docs)=>{
