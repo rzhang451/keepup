8a8171be4e4c66914a65bcb49bc8fff68c50840b
ysong02 <71662449+ysong02@users.noreply.github.com>
2021-05-23 00:39:13 +0200

    Create add_comment.js

---
+++ Back-end/routes/add_comment.js
@@ -0,0 +1,23 @@
+const Profile = require('../model/profileModel');
+const Media = require('../model/mediaModel');
+
+exports.add_comment = (req,res,next)=>{
+  Media.find({id: req.body.id},(err,docs)=>{
+    if(err){
+      return res.json({
+        msg:'Failed to connect',
+        code: 'error'
+      });
+      if(!docs.length){
+      //返回动态不存在
+        return res.json({
+          msg:'Diary doesn\'t existe',
+          code: 'error'
+        });
+      }
+    }else{
+     //add new comment
+      var comment_usr = req.body.usr;
+      var comment_content = req.body.content;
+      comment.add(comment_usr,comment_content);
+    }
