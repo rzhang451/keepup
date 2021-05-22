f4a9e0bdcfe40a8689a60bd2cfc739a12eeb3caf
LIU YIHENG <77144103+samsara-kama@users.noreply.github.com>
2021-05-23 00:48:10 +0200

    Add files via upload

---
+++ Back-end/routes/affiche_pyq_follow.js
@@ -0,0 +1,40 @@
+const Follow=require('../model/followModel');
+const Media=require('../model/mediaModel');
+
+exports.affiche_pyq_follow=(req,res,next)=>{
+  Follow.findOne({id:req.params.id},(err,docs)=>{
+    if(err){
+      return res.json({
+        msg:'failed to connect',
+        code:'error'
+      });
+    }
+    if(!docs){
+      return res.json({
+        msg:'Cannot find user',
+        code:'error'
+      });
+    }
+    var follow={
+      follow:docs.follow,
+      follower:docs.follower,
+    };
+    var info= [] ;
+    var i;
+    var j;
+    for(i=0;i<follow[follow][i];i++){
+      Media.find({id:follow[follow][i]},(err,actus)=>{
+        if(err){
+          return res.json({
+            msg:'Failed to connect',
+            code: 'error'
+          });
+        }
+        while(j<actus.length){
+          info.append(actus[j]);
+          j++;
+        }
+      });
+    }
+  })
+}
---
+++ Back-end/routes/affiche_pyq_self.js
@@ -0,0 +1,46 @@
+const Media=require('../model/mediaModel');
+const Profile=require('../model/profileModel');
+
+exports.affiche_pyq_self=(req,res,next)=>{
+  Profile.findOne({id:req.params.id},(err,docs)=>{
+    if(err){
+      return res.json({
+        msg:'Failed to connect',
+        code: 'error'
+      });
+    }
+    if(!docs){
+      return res.json({
+        msg:'Username incorrect!',
+        code: 'error'
+      });
+    }
+    var profile = {
+      username: docs.username,
+      sex: docs.sex,
+      location: docs.location,
+      miniIntro: docs.miniIntro
+    };
+    var info = [];
+    Media.find({id:req.params.id},(err,actus)=>{
+      if(err){
+        return res.json({
+          msg:'Failed to connect',
+          code: 'error'
+        });
+      }
+      var i=0;
+      while(i<actus.length){
+        info.append(actus[i]);
+        i++;
+      }
+    });
+    return res.json({
+      msg:'Profile is returned',
+      code:'success',
+      profile:profile,
+      info:info
+    });
+  });
+  next();
+}
