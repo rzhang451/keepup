c28f1b0b5da4acf81a2ada3303a49a14c707c0e6
ShawnYWY <ywy.shawn@gmail.com>
2021-05-23 18:01:41 +0200

    Create search.js

---
+++ Back-end/routes/search.js
@@ -0,0 +1,44 @@
+const Course = require('../model/courseModel');
+const Profile = require('../model/profileModel');
+
+exports.search = (req,res,next)=>{
+    Course.findOne({name:req.params.name},(err,docs)=>{
+        if(err){
+          return res.json({
+            msg:'failed to connect',
+            code:'error'
+          });
+        }
+        if(!docs){
+          return res.json({
+            msg:'Cannot find course',
+            code:'error'
+          });
+        }
+        return res.json({
+            msg:'Course is returned',
+            code:'success',
+            Course:docs.id
+        });
+    });
+    Profile.findOne({id:req.params.id},(err,docs)=>{
+        if(err){
+          return res.json({
+            msg:'Failed to connect',
+            code: 'error'
+          });
+        }
+        if(!docs){
+          return res.json({
+            msg:'Username incorrect!',
+            code: 'error'
+          });
+        }
+        return res.json({
+          msg:'Profile is returned',
+          code:'success',
+          Profile:docs.username
+        });
+    });
+    next();
+}
