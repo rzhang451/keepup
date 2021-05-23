85f5e0242190be82d552e594d0bd28001858132a
ysong02 <71662449+ysong02@users.noreply.github.com>
2021-05-23 22:54:10 +0200

    add comment time

--- Back-end/routes/add_comment.js
+++ Back-end/routes/add_comment.js
@@ -20,7 +20,7 @@ exports.add_comment = (req,res,next)=>{
       var comment_usr = req.body.usr;
       var comment_content = req.body.content;
       var comment_date = Date();
-      comment.add(comment_usr,comment_content);
+      comment.add(comment_usr,comment_content,comment_date);
     }
   })
 }
