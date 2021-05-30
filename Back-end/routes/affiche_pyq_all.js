aca1af18bdbb6414d3f98c39b50c07522d369f39
LIU YIHENG <77144103+samsara-kama@users.noreply.github.com>
2021-05-30 23:33:52 +0200

    Update affiche_pyq_all.js

--- Back-end/routes/affiche_pyq_all.js
+++ Back-end/routes/affiche_pyq_all.js
@@ -1,47 +1,52 @@
 const Profile=require('../model/profileModel');
 const Media=require('../model/mediaModel');
-
 exports.affiche_pyq_all=(req,res,next)=>{
-  Profile.find({},(err,docs)=>{
-    if(err){
-      return res.json({
-        msg:'failed to connect',
-        code:'error'
-      });
-    }
-    if(!docs){
-      return res.json({
-        msg:'Cannot find user',
-        code:'error'
-      });
-    })
-    var ids = [];
-    var i;
-    for(i=0;i<docs.length;i++){
-      ids[i].append(docs.id);
-    }
-    var j;
-    var info = [];
-    for(j=0;j<docs.length;j++){
-      Media.find({id:ids[j]},(err,actus)=>{
+  const date = new Date();
+  var j;
+  var infos = [];
+  for(j=0;j<30;j++){
+      Media.find({date:{$gte:date.setDate(date.getDate()-5)}},(err,actus)=>{
         if(err){
           return res.json({
             msg:'Failed to connect',
-            code: 'error'
+            code: '-1'
           });
         }
-        while(j<actus.length){
-          info.append(actus[j]);
-          j++;
+        if(j==30){
+          req.session.date=date;
         }
       });
-    }
-    return res.json({
-      msg:'All media is returned',
-      code:'success',
-      profile:profile,
-      info:info
-    });
+      Profile.find({id:actus.id},(err,docs)=>{
+        if(err){
+          return res.json({
+            msg:'failed to connect',
+            code:'error'
+          });
+          }
+        if(!docs){
+         return res.json({
+            msg:'Cannot find user',
+            code:'-1'});
+        }
+      });
+      var data={
+          id:actus.id,
+          username:actus.usr,
+          avatar:docs.avatar,
+          date:actus.date,
+          message:actus.content,
+          like:actus.like,
+          like_user:actus.like_user,
+          photo:actus.pic
+      };
+      datas.push(info);
+      }
+      res.json{        
+        msg:'All media is returned',
+        code:'200',
+        data: datas}
+    
+      
   next();
 }
 }
