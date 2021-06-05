5eeea5a672b905f0cd63cbcfd0aaddbfad9a498c
ysong02 <71662449+ysong02@users.noreply.github.com>
2021-06-05 20:11:53 +0200

    Update profileModel.js

--- Back-end/model/profileModel.js
+++ Back-end/model/profileModel.js
@@ -3,27 +3,49 @@ const mongoose = require('./mongoose');
 const uuid = require('node-uuid');
 //Profile database
 var profileSchema = mongoose.Schema({
-  id: String,
+  id:{
+    type:String,
+    default: uuid.v1
+  },
   username:{
     type:String,
     default: uuid.v1
   },
   email: String,
   password: String,
-  sexe: String,
-  location: String,
-  miniIntro: String,
-  Height: Number,
-  Weight: Number,
-  Bmi: Number,
+  sexe: {
+    type:String,
+    default:'secret'
+  },
   avatar:{
     type:String,
     default:'/public/upload/image/profile/default.jpg'
   },
+  location:{
+    type:String,
+    default: 'beijing'
+  },
+  miniIntro: {
+    type:String,
+    default:'hello'
+  },
+  Height: {
+    type:Number,
+    default: 180
+  },
+  Weight: {
+    type:Number,
+    default: 70
+  },
+  Bmi: {
+    type:Number,
+    default: 21.60
+  },
   totalExerciceDay: Number,
   totalExerciceTime: Number,
   totalConsumption: Number,
-  favor_course:[String]
+  favor_course:[String],
+  favor_date:[String]
 });
 
 //Model
