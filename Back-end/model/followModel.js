7c6a711d2485b5c8507582b19ae600e675902b38
ysong02 <71662449+ysong02@users.noreply.github.com>
2021-06-05 20:08:42 +0200

    Update followModel.js

--- Back-end/model/followModel.js
+++ Back-end/model/followModel.js
@@ -3,8 +3,25 @@ const mongoose = require('./mongoose');
 //My Course database
 var followSchema = mongoose.Schema({
   id: String,
-  follow: [String],
-  follower: [String]
+  follow: [{
+    id: String,
+    name: String,
+    avatar: String
+  }],
+  follower: [{
+    id: String,
+    name: String,
+    avatar: String
+  }],
+  follow_num:{
+    type:Number,
+    default:0
+  },
+  follower_num:{
+    type:Number,
+    default:0
+  }
 });
+
 
 module.exports = mongoose.model('Follow',followSchema);
